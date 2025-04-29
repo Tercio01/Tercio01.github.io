4396c29f3c27afb8a4c2d5b4433d5add9295bb9f
Tercio01 <tercio1parente@gmail.com>
2025-04-29 08:43:51 -0300

    potfolio

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
-
-document.getElementById('contact-form').addEventListener('submit', function(event) {
-        event.preventDefault();
-        alert('Mensagem enviada com sucesso!');
-    });
-    
+document.getElementById('contact-form').addEventListener('submit', function(event){
+    event.preventDefault();
+    alert('Mensagem enviada com sucesso!');
+});
